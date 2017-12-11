f31447d785d9095735edb54089c7893d90543336
ngratzi <ngratzi@g.clemson.edu>
2017-12-10 21:29:43 -0800

    updated package.json

--- social_graph_service/server/index.js
+++ social_graph_service/server/index.js
@@ -8,21 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/tweets/events', (req, res) => {
-  res.send();
-});
 
-
-app.get('/interactors/:tweet_id', (req, res) => {
-  //console.log('tweet_id', Path.parse(req.path).base);
-  res.send();
-});
-
-
-app.post('/friends/:user_id', (req, res) => {
-  //console.log('user_id', Path.parse(req.path).base);
-  res.send();
-});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
 
